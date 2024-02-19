90f1946f420a0d9f20169d7078dc43aca6333adf
GraviDezucy <nhtduc1604@gmail.com>
2024-02-19 16:04:26 +0700

    lastest update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import {Component} from '@angular/core';
+import {HomeComponent} from './home/home.component';
+import {RouterLink, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent, RouterLink, RouterOutlet],
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular100days';
-
-  inputType = 'text';
-
-  user = {
-    name: 'Dezucy',
-    age: '24'
-  };
-
-  handler() {
-    console.log("Clicked");
-  }
+  title = 'homes';
 }
