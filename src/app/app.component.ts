9ee4632bc74641851c282adb02ed877fc763886a
GraviDezucy <nhtduc1604@gmail.com>
2024-02-27 15:58:06 +0700

    {{}},1 way [],2 ways  [()]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloComponent],
+  imports: [RouterOutlet, HelloComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular100day';
+
+  inputType = 'text';
+
+  user = {
+    name: 'Dezucy',
+    age: 24,
+  };
+
+  Click(event: any) {
+    console.log('Clicked', event);
+  }
 }
