be894dd3f304d0c8dbda22ee4f4e73a63709bcfc
GraviDezucy <nhtduc1604@gmail.com>
2024-02-27 16:53:56 +0700

    ngIF, hidden

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloComponent, FormsModule],
+  imports: [RouterOutlet, HelloComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular100day';
-
-  inputType = 'text';
+  show = false;
 
   user = {
     name: 'Dezucy',
-    age: 24,
+    age: 19,
   };
-
-  Click(event: any) {
-    console.log('Clicked', event);
-  }
 }
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   standalone: true,
   imports: [],
   templateUrl: './hello.component.html',
-  styleUrl: './hello.component.css'
+  styleUrl: './hello.component.css',
 })
 export class HelloComponent {
+  @Input() name: String | undefined;
 
+  ngOnInit() {
+    console.log('Hello Init');
+  }
+
+  ngOnDestroy() {
+    console.log('Hello Destroy');
+  }
 }
